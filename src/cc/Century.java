e08712a39502248d5fe43ecd9b716e3e7aaef9cd
toby jessup <toby.jessup@homeoffice.gov.uk>
2020-04-03 17:13:37 +0100

    Updated to prevent incorrect characters being entered at any point, along with an added loop so user can find a century for a given year until they wish to exit. Also cleaned up the code.

--- src/cc/Century.java
+++ src/cc/Century.java
@@ -1,5 +1,7 @@
 package cc;
 
+import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Century {
@@ -8,38 +10,52 @@ public class Century {
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        //Scanner
+        //Scanner to take year entered and output year.java
         Scanner readme = new Scanner(System.in);
         System.out.println("Enter year then press enter to find century:");
-        //two variables to hold numbers
-        int year1, display;
-        year1 = readme.nextInt();
-        display = year1;
-        //System.out.println("Total = " + year);
+        int year1;
+        char choice;
 
-        //int count = 0;
-        int givenYear = year1;
-
-        //for (int i=startYear; i<= endYear; i++) {
-
-        //blankLine("counter "+ count++);
-        Year year = new Year(year1);
-        year.displayYear();
-        year.displayCentury();
-        //blankLine();
-    }
+        //try catch to prevent invalid characters being entered and allowing for a handling process
+        try {
+            year1 = readme.nextInt();
+            Year year = new Year(year1);
+            year.displayYear();
+            year.displayCentury();
+        } catch (InputMismatchException ex) {
+            //println's below because there is already an error message in the if statement if y or n not entered
+            /*System.out.println("This value is not valid, please enter a year.");
+            System.out.println("Would you like to enter a new year? (Y/N)");*/
+        }
+        //do while loop to allow for the program to continue running as long as the user wishes along with continued inputmismatch handling
+        do {
+        {
+            choice = readme.next().charAt(0);
+        }
+        {
+            if ((choice == 'y') || (choice == 'Y')) {
+                System.out.println("Please enter the new year you wish to find the century for:");
+                    try {
+                        year1 = readme.nextInt();
+                        Year year = new Year(year1);
+                        year.displayYear();
+                        year.displayCentury();
+                    } catch (InputMismatchException ex) {
+                        //println's below because there is already an error message in the if statement if y or n not entered
+                        /*System.out.println("This value is not valid, please enter a year.");
+                        System.out.println("Would you like to enter a new year? (Y/N)");*/
+                    }
+            } else if ((choice == 'n') || (choice == 'N')) {
+                break;
+                //System.out.println("Thank you for using this tool.");
+            } else {
+                System.out.println("This character is invalid, would you like to enter a new year? (Y/N)");
+            }
+        }
+    } while (true);
+        System.out.println("Thank you for using this tool.");
 }
-
-// private static void blankLine() {
-// System.out.println();
-//
-//  private static void blankLine(String string) {
-//     System.out.println(string);
-//   }
-//}
+}
--- src/cc/Year.java
+++ src/cc/Year.java
@@ -1,33 +1,38 @@
 package cc;
 
+import java.util.InputMismatchException;
+
 public class Year {
 
     int year = 0;
 
 
     public int getCentury() {
-        /*if (year <= 100) {
-            return 1;
-        } else {
-            return (year /100)+1;
-        }*/
 
-        if (year <= 0) {
-            return 0;
-        } else if (year <= 100) {
-            return 1;
-        } else if (year % 100 == 0){
-            return (year /100);
-        } else {
-            return (year / 100) + 1;
+        try {
+            if (year == 0) {
+                return 0;
+            } else if (year < 0) {
+                //throw new added to prevent negative numbers being entered into the century calculator.
+                throw new IllegalArgumentException("Value must be non-negative.");
+            } else if (year <= 100) {
+                return 1;
+            } else if (year % 100 == 0) {
+                return (year / 100);
+            } else {
+                return (year / 100) + 1;
+            }
+        } catch (IllegalArgumentException i) {
+            System.out.println("The year entered must not be negative.");
         }
-    }
+    return 0; }
 
     public void displayYear () {
-        System.out.println("The Year is " + year);
+        System.out.println("The year you have entered is: " + year);
     }
 
     public void displayCentury () {
+        //Block comment below was an attempt to handle the endings on different centuries opted for the below option which will need updating to allow for all numbers
         /*Interger parse.int
 
           System.out.println("The Century is " + getCentury() + "st");
@@ -41,18 +46,22 @@ public class Year {
         }*/
         int century = getCentury();
 
-        if (century == 1 || century == 21) {
-            System.out.println("The Century is " + century + "st");
-        } else if (century == 2 || century == 22) {
-            System.out.println("The Century is " + century + "nd");
-        } else if (century == 3 || century == 23) {
-            System.out.println("The Century is " + century + "rd");
-        //} else if (century == 21) {
-            //System.out.println("The Century is " + century + "st");
-        } else {
-            System.out.println("The Century is " + century + "th");
+            if (century == 1 || century == 21) {
+                System.out.println("The century for the given year is: " + century + "st");
+            } else if (century == 2 || century == 22) {
+                System.out.println("The century for the given year is: " + century + "nd");
+            } else if (century == 3 || century == 23) {
+                System.out.println("The century for the given year is: " + century + "rd");
+                //} else if (century == 21) {
+                //System.out.println("The Century is " + century + "st");
+            } else if (century == 0){
+                System.out.println("The century for the given year is not available.");
+            } else {
+                System.out.println("The century for the given year is: " + century + "th");
+            }
+
+            System.out.println("Would you like to enter a new year? (Y/N)");
         }
-    }
 
     public long getYear() {
         return year;
